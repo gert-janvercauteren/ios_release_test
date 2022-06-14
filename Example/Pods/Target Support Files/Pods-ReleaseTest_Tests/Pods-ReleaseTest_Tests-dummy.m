1c9dc9821cb41a2eb83c95789fb21831d13f12ea
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2022-06-14 11:33:22 +0800

    added back Example

---
+++ Example/Pods/Target Support Files/Pods-ReleaseTest_Tests/Pods-ReleaseTest_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReleaseTest_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ReleaseTest_Tests
+@end
---
+++ Example/Pods/Target Support Files/ReleaseTest/ReleaseTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ReleaseTest : NSObject
+@end
+@implementation PodsDummy_ReleaseTest
+@end
